d9d6519f1a8b3c25e08fac749c189d8f6911417e
gowtdy <gowtd@gmail.com>
2026-01-05 18:38:50 +0800

    mtserver

--- translate-image/mtserver-init.sh
+++ translate-image/mtserver-init.sh
@@ -2,7 +2,7 @@ echo "Initializing mtserver"
 
 sudo mkdir -p /home/aigc/data/aioutput/mtserver-logs/mtserver
 sudo chown -R aigc:aigc /home/aigc/data/aioutput/mtserver-logs/mtserver
-sudo chmod -R 755 /home/aigc/data/aioutput/mtserver-logs/mtserver
+sudo chmod -R 755 /home/aigc/data/aioutput/mtserver-logs
 
 echo "mtserver initialized"
 ls -la /home/aigc/data/aioutput/mtserver-logs/
