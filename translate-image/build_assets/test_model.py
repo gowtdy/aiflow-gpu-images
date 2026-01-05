453b68a27d9c640c6d5fa4c725de92f0ebabdbab
gowtdy <gowtd@gmail.com>
2026-01-05 21:22:59 +0800

    load models

--- translate-image/build_assets/test_model.py
+++ translate-image/build_assets/test_model.py
@@ -3,8 +3,13 @@ import os
 
 model_path = "/app/models/mt_models"
 
+print(f"Loading model from {model_path}")
+print(f"Loading tokenizer from {model_path}")
 tokenizer = AutoTokenizer.from_pretrained(model_path)
+print(f"Loading model from {model_path}")
 model = AutoModelForCausalLM.from_pretrained(model_path, device_map="auto")  # You may want to use bfloat16 and/or move to GPU here
+print(f"Model loaded")
+print(f"Loading messages")
 messages = [
     {"role": "user", "content": "Translate the following segment into Chinese, without additional explanation.\n\nIt’s on the house."},
 ]
@@ -16,4 +21,6 @@ tokenized_chat = tokenizer.apply_chat_template(
 )
 
 outputs = model.generate(tokenized_chat.to(model.device), max_new_tokens=2048)
+print(f"Generating outputs: {outputs}")
 output_text = tokenizer.decode(outputs[0])
+print(f"Output text: {output_text}")
