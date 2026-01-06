75932a123467fa51a97ec8967790ccf6a1c43dc0
gowtdy <gowtd@gmail.com>
2026-01-06 09:42:09 +0800

    fastapi

--- translate-image/build_assets/start_mt_server.sh
+++ translate-image/build_assets/start_mt_server.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
 
 echo "Starting mt_worker"
-export CUDA_VISIBLE_DEVICES=${GPU_INDEX:-0}
-echo "Using GPU: ${CUDA_VISIBLE_DEVICES}"
-python3 -m vllm.entrypoints.openai.api_server --model /app/models/mt_models \
-  --host 0.0.0.0 \
-  --port ${APP_PORT:-5400} \
-  --tensor-parallel-size ${TENSOR_PARALLEL_SIZE:-1} \
-  --dtype bfloat16 \
-  --max-model-len 1024 \
-  --gpu_memory_utilization 0.5 \
-  --served-model-name mt-model \
-  --trust-remote-code \
-  2>&1 | tee /app/log/mt_server.log
+#export CUDA_VISIBLE_DEVICES=${GPU_INDEX:-0}
+#echo "Using GPU: ${CUDA_VISIBLE_DEVICES}"
+
+PORT=${APP_PORT:-5000}
+WORKERS=${WORKERS:-3}
+#python3 -m vllm.entrypoints.openai.api_server --model /app/models/mt_models \
+#  --host 0.0.0.0 \
+#  --port ${APP_PORT:-5400} \
+#  --tensor-parallel-size ${TENSOR_PARALLEL_SIZE:-1} \
+#  --dtype bfloat16 \
+#  --max-model-len 1024 \
+#  --gpu_memory_utilization 0.5 \
+#  --served-model-name mt-model \
+#  --trust-remote-code \
+#  2>&1 | tee /app/log/mt_server.log
+
+uvicorn gpu_mtserver:app --host 0.0.0.0 --port "$PORT" --workers "${WORKERS}"
