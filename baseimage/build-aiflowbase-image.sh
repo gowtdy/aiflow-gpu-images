a5caf0046a1926a6a4be584567fd599bc03000b1
gowtdy <gowtd@gmail.com>
2026-03-01 23:32:42 +0800

    Dockerfile

--- baseimage/build-aiflowbase-image.sh
+++ baseimage/build-aiflowbase-image.sh
@@ -1,4 +1,5 @@
 docker build -t aiflowbase:0.3 -f Dockerfile .
 
+# Run with host aigc UID/GID: docker run -e AIGC_UID=$(id -u aigc) -e AIGC_GID=$(id -g aigc) -it aiflowbase:0.3 ...
 #docker build -t 100.95.222.4:2000/aiflowbase:0.1 -f Dockerfile .
 #docker push 100.95.222.4:2000/aiflowbase:0.1
