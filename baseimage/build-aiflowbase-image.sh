f4f69aa2be0a71691c072775039918f3853b3321
gowtdy <gowtd@gmail.com>
2026-03-25 11:04:11 +0800

    flash attn 2.0 delete

--- baseimage/build-aiflowbase-image.sh
+++ baseimage/build-aiflowbase-image.sh
@@ -1,4 +1,4 @@
-docker build -t aiflowbase:0.4 -f Dockerfile .
+docker build -t aiflowbase:0.4.1 -f Dockerfile .
 
 # Run with host aigc UID/GID: docker run -e AIGC_UID=$(id -u aigc) -e AIGC_GID=$(id -g aigc) -it aiflowbase:0.3 ...
 #docker build -t 100.95.222.4:2000/aiflowbase:0.1 -f Dockerfile .
