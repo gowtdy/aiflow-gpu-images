53917f9416829b649f572f691c3974e4a396fb9e
gowtdy <gowtd@gmail.com>
2026-07-07 16:26:40 +0800

    requirement

--- gpu50-baseimage/build-gpu50-baseimage.sh
+++ gpu50-baseimage/build-gpu50-baseimage.sh
@@ -1,4 +1,4 @@
-docker build -t gpu50-baseimage:0.1 -f Dockerfile .
+docker build -t gpu50-baseimage:0.2 -f Dockerfile .
 
 # Run with host aigc UID/GID: docker run -e AIGC_UID=$(id -u aigc) -e AIGC_GID=$(id -g aigc) -it aiflowbase:0.3 ...
 #docker build -t 100.95.222.4:2000/aiflowbase:0.1 -f Dockerfile .
