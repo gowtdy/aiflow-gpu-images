50bb3b2be39c00782b38dd859805153f6d542234
gowtdy <gowtd@gmail.com>
2026-07-13 15:25:02 +0800

    builder

--- video-image/htmlvideo-image/hyperframes-local/build-hyperframes-image.sh
+++ video-image/htmlvideo-image/hyperframes-local/build-hyperframes-image.sh
@@ -1,2 +1,2 @@
-# Build ttsbaseimage (FROM aiflowbase:0.4). Run 時由 base entrypoint 依 AIGC_UID/AIGC_GID 建立 aigc，B 機可傳 -e AIGC_UID=$(id -u aigc) -e AIGC_GID=$(id -g aigc)，未傳則預設 1001。
-docker build --progress=plain -t hyperframes-origin-image:0.1 -f Dockerfile .
+# Build hyperframes-local image (FROM gpu50-baseimage:0.1). Run 時由 base entrypoint 依 AIGC_UID/AIGC_GID 建立 aigc，B 機可傳 -e AIGC_UID=$(id -u aigc) -e AIGC_GID=$(id -g aigc)，未傳則預設 1001。
+docker build --progress=plain -t hyperframes-local-image:0.1 -f Dockerfile .
