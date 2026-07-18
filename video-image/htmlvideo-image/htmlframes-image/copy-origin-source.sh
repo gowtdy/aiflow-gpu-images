408355dbdd5126ab2d0a2624959a006a10bc4e50
gowtdy <gowtd@gmail.com>
2026-07-18 10:52:49 +0800

    brief style preset

--- video-image/htmlvideo-image/htmlframes-image/copy-origin-source.sh
+++ video-image/htmlvideo-image/htmlframes-image/copy-origin-source.sh
@@ -2,7 +2,8 @@
 
 echo "copy origin source"
 origin_source_path=/Users/gowtd/work/src/open/video/hyperframes
-target_path=build_assets/hyperframes
+build_assets_path=build_assets
+target_path=${build_assets_path}/hyperframes
 bak_path=hyperframe_bak
 echo "mv ${target_path} ${bak_path}"
 mv ${target_path} ${bak_path}
@@ -22,6 +23,8 @@ echo "mkdir -p ${target_path}/skills"
 mkdir -p ${target_path}/skills
 cp -r ${origin_source_path}/skills/hyperframes-cli ${target_path}/skills
 cp -r ${origin_source_path}/skills/hyperframes-core ${target_path}/skills
+cp -r ${origin_source_path}/skills/hyperframes-creative ${target_path}/skills
+cp -r ${origin_source_path}/skills/faceless-explainer/scripts/build-frame.mjs ${build_assets_path}/scripts/
 cp ${origin_source_path}/bun.lock ${origin_source_path}/package.json ${origin_source_path}/CLAUDE.md ${origin_source_path}/AGENTS.md ${target_path}
 
 echo "patch telemetry"
