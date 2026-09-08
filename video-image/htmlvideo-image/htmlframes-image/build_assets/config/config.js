c3f27233495c86427fc22c0541c5d90f2595f12b
gowtdy <gowtd@gmail.com>
2026-09-08 12:45:12 +0800

    add

---
+++ video-image/htmlvideo-image/htmlframes-image/build_assets/config/config.js
@@ -0,0 +1,18 @@
+export const config = {
+  signature: {
+    secretKey: "0x4AAAAAABL7dEKVA5-OlS1S",
+    soundSecretKey: "Kvy49gPaoDbiIojFmENTIYKgzE35DiqaTzZegEh5z+M=",
+    uploadSecretKey: "ACpz51zjj2OAi/FqFeut0d9nq7XnTn63/mzQyOO8hgM=",
+    coverSecretKey: "l4AnBbVDNIardm33PyFP0Ki29uLAn9a6xrzNL/6FPuQ=",
+  },
+  host: "http://100.95.222.4:7089",
+  apiMethod: "/api/genaudio",
+  tts: {
+    email: "video_script_tts",
+    userid: "clm-default",
+    domain: "caluma.ai",
+    subscript: 1,
+    t: 1,
+    defaultVoice: "english|voice-lady-female",
+  },
+};
